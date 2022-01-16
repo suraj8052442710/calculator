93d03d2674c00c25deb10a77f1ed005b2c55d512
suraj <sag93186@gmail.com>
2022-01-16 22:28:28 +0530

    initial project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+float number = 500;
+string text = "suraj";
+char letter = 'a';
+bool isTrue = true;
+float numbertwo = 1.5f;
+Console.WriteLine(number);
+Console.WriteLine(text);
+float number3 = 1000;
+float result = number / number3;
+Console.WriteLine(result);
+ text=Console.ReadLine();
+Console.WriteLine(text);
